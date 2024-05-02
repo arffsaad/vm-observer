b82b8e0e3f3f102db2b535b7b53d9d1f0f764ed3
Ariff Saad <80538339+arffsaad@users.noreply.github.com>
2024-05-02 11:58:12 +0800

    golang - finish producer part, still contain hardcoded info. TODO cater for bash-script installation.

--- collector-agent-app/collector.go
+++ collector-agent-app/collector.go
@@ -1,33 +1,73 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"encoding/json"
+	"log"
 	"os"
+	"time"
 
+	"github.com/segmentio/kafka-go"
 	"github.com/shirou/gopsutil/disk"
 )
 
-func main() {
+type DiskUsage struct {
+	Host   string         `json:"hostname"`
+	HostID string         `json: "hostID"`
+	Disk   map[string]int `json:"disk"` // Map for named disk usage
+}
+
+func updateUsage() {
 	host, err := os.Hostname()
 	if err != nil {
-		fmt.Println("Error retrieving hostname!")
-		panic(err)
-	}
-	fmt.Println("Usage statistics for", host)
-	parts, err := disk.Partitions(false)
-	if err != nil {
-		fmt.Println("Error retrieving partitions!")
-		panic(err)
-	}
-
-	for _, partition := range parts {
-		fmt.Println("Partition: ", partition)
+		log.Fatal("Error retrieving hostname!", err)
 	}
 
 	usg, err := disk.Usage("/")
 	if err != nil {
-		fmt.Println("Error retrieving usage!")
-		panic(err)
+		log.Fatal("Error retrieving usage!", err)
 	}
-	fmt.Println("Main disk usage (~/):", usg)
+
+	disk := &DiskUsage{
+		Host:   host,
+		HostID: "b2626db1-879f-478c-9e0b-56060e5d6912", // hardcoded, this will be set by installation bash script
+		Disk:   map[string]int{"total": int(usg.Total), "free": int(usg.Free), "used": int(usg.Used)},
+	}
+
+	usgData, err := json.Marshal(disk)
+	if err != nil {
+		log.Fatal("Unable to process usage statistics!", err)
+	}
+
+	topic := "deviceMetrics_someuuid-213123123-asdfasdvkj" // will also be set by bash script.
+	partition := 0
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", topic, partition)
+	if err != nil {
+		log.Fatal("Failed to dial leader:", err)
+	}
+
+	conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+	_, err = conn.WriteMessages(
+		kafka.Message{Value: []byte(usgData)},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := conn.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+}
+
+func main() {
+	ticker := time.NewTicker(time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			go updateUsage()
+		}
+	}
 }
--- collector-agent-app/go.mod
+++ collector-agent-app/go.mod
@@ -2,10 +2,15 @@ module rc-assessment/collector-agent
 
 go 1.22.2
 
-require github.com/shirou/gopsutil v3.21.11+incompatible
+require (
+	github.com/segmentio/kafka-go v0.4.47
+	github.com/shirou/gopsutil v3.21.11+incompatible
+)
 
 require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 )
